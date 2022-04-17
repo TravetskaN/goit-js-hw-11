1595792562eb8e6984152e75717ee3962eb7fe93
TravetskaN <ntravetskaya@gmail.com>
2022-04-17 14:42:46 +0100

    дз

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { form, input, gallery, moreButton } from './js/refs';
+import getImages from './js/getimages';
+import galleryTemplate from './templates/galleryTemplate.hbs';
+
+import './sass/main.scss';
+
+let inputValue = '';
+let pageCounter = 1;
+
+const createMarkup = event => {
+  event.preventDefault();
+
+  getImages(inputValue, pageCounter).then(photos => (gallery.innerHTML = galleryTemplate(photos)));
+
+  pageCounter = 1;
+
+  if (!inputValue) {
+    moreButton.classList.remove('visible-button');
+  }
+};
+
+input.addEventListener('input', ({ currentTarget }) => {
+  inputValue = currentTarget.value.trim();
+});
+
+moreButton.addEventListener('click', () => {
+  pageCounter += 1;
+
+  getImages(inputValue, pageCounter).then(photos =>
+    gallery.insertAdjacentHTML('beforeend', galleryTemplate(photos)),
+  );
+});
+
+form.addEventListener('submit', createMarkup);
+
+
